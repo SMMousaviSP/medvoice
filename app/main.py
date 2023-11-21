51fbcc3b12394e709f971ed655ef39b4397875b0
SMMousaviSP <smmousavisp@gmail.com>
2023-11-21 21:32:52 +0100

    Handle errors by returning JSON

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, json, jsonify
+from werkzeug.exceptions import HTTPException
 
 
 app = Flask(__name__)
 
 
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "data": {},
+        "errors": [e.description],
+    })
+    response.content_type = "application/json"
+    return response
+
+
 @app.route("/", methods=["GET"])
 def hello_world():
-    return {"message": "Hello, World!"}, 200
+    return jsonify({"message": "Hello, World!"}), 200
 
 
 if __name__ == '__main__':
